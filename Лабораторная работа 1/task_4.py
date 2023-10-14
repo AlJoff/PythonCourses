7d7a09fdbd6b9baf4ba185d70ea3c8056b617620
AlJoff <146883182+AlJoff@users.noreply.github.com>
2023-10-15 02:01:39 +0300

    Update task_4.py

--- Лабораторная работа 1/task_4.py
+++ Лабораторная работа 1/task_4.py
@@ -1,12 +1,12 @@
 users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
 
 # TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
-siteVisitStatistics = {
+site_visit_statistics = {
     "Общее количество": 0,
     "Уникальные посещения": 0
 }
 
-siteVisitStatistics["Общее количество"] = len(users)
-siteVisitStatistics["Уникальные посещения"] = len(set(users))
+site_visit_statistics["Общее количество"] = len(users)
+site_visit_statistics["Уникальные посещения"] = len(set(users))
 
-print(siteVisitStatistics)
+print(site_visit_statistics)
