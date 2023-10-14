f4bf5f22a7bad38448c5a834a3bea939323d8e21
AlJoff <146883182+AlJoff@users.noreply.github.com>
2023-10-15 01:56:58 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,16 +1,16 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-CHAR_SIZE = 4                   # Размер одного символа в байтах
+CHAR_SIZE = 4                       # Размер одного символа в байтах
 
-floppyCapacitySpaceMb = 1.44    # Объем дискеты в Мб
-bookPageCount = 100             # Количество страниц в книге
-linesPerPage = 50               # Число строк на странице
-charactersInLine = 25           # Количество символов в строке
+floppy_capacity_space_mb = 1.44     # Объем дискеты в Мб
+book_page_count = 100               # Количество страниц в книге
+lines_per_page = 50                 # Число строк на странице
+characters_in_line = 25             # Количество символов в строке
 
-charactersInBook = bookPageCount * linesPerPage * charactersInLine  # Количество симовлов в книге
+characters_in_book = book_page_count * lines_per_page * characters_in_line  # Количество симовлов в книге
 
-bookSizeBytes = charactersInBook * CHAR_SIZE                # Размер книги в байтах
-floppyCapacitySpaceBytes = floppyCapacitySpaceMb*1024*1024  # Объем дискеты в байтах
+book_size_bytes = characters_in_book * CHAR_SIZE                        # Размер книги в байтах
+floppy_capacity_space_bytes = floppy_capacity_space_mb * 1024 * 1024    # Объем дискеты в байтах
 
-bookOnFloppy = int(floppyCapacitySpaceBytes / bookSizeBytes)
+book_on_floppy = int(floppy_capacity_space_bytes / book_size_bytes)
 
-print("Количество книг, помещающихся на дискету:", bookOnFloppy)
+print("Количество книг, помещающихся на дискету:", book_on_floppy)
